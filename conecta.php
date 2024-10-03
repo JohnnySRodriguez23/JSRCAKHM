e58dee7135faac05fde13dc287b8b81163c0b106
JohnnySRodriguez23 <jr3414973@gmail.com>
2024-10-02 22:42:54 -0600

    JSRCAKHM

---
+++ conecta.php
@@ -0,0 +1,6 @@
+<?php
+$NameSEVER"localhost";//nombre del servicidor
+$UserName="root"//Nombre del Usuario de SGBD
+$Password="1234"//Password del SGBD
+$DBname="talk";
+//Crear la conexion a la base de datos
---
+++ registro.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <meta name="viewport" conect="width=divice-width, initial-scale=1.0">
+    <title>Registro de Ususario</title>
+</head>
+<body>
+    <div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+                <h2 class="display-6 text-cemtrer mt-2">Registro de usuarios</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST">
+
+               <div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+                <h2 class="display-6 text-cemtrer mt-2">Nombre</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST">
+                <h2 class="display-6 text-cemtrer mt-2">ApellidoP</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST"><div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+                <h2 class="display-6 text-cemtrer mt-2">ApellidoM</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST"><div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+                <h2 class="display-6 text-cemtrer mt-2">Telefono</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST"><div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+                <h2 class="display-6 text-cemtrer mt-2">Email</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST"><div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+                <h2 class="display-6 text-cemtrer mt-2">Contraseña</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST"><div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST"><div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+                <h2 class="display-6 text-cemtrer mt-2">Boton</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>"method="POST"><div class="container justify-conten-center">
+        <div class="col-sm10 col-md 10 clos-gm10">
+            <div class="card shadow mt-5">
+            </div>a></div>
+    </div>
+</body>
+</html>
